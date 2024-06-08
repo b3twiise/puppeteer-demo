c067b69baee5d430a5be1bbf0eec52cfe81f2d73
b3twiise <b3twiise@gmail.com>
2024-06-08 03:16:59 -0400

    grabbed scripts

--- index.js
+++ index.js
@@ -1,18 +1,75 @@
 import puppeteer from 'puppeteer';
-import * as fs from 'node:fs'
+import * as fs from 'node:fs';
+
+const writeToFile = (fname)  => {
+    fs.writeFile( `./src/${fname}`, 'utf-8', (err) => {
+        if (err) {
+          console.error('Error dumping body:', err);
+        } else {
+          console.log('Successful write for scripts');
+        }
+      });
+
+ }
+
 (async () => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   // Navigate the page to a URL
-  await page.goto('https:rtb.bf');
-  await page.setViewport({width: 1080, height: 1024});
-  let html = await(page.content())
-  fs.writeFile( "body.html", html, "utf-8", (err, success) => {
-    if(err) console.log("Error dumping bdoby")
-    if(success) console.log("Successfull write")
-  } )
+  await page.goto('https://rtb.bf');
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  // Get the page content
+  const html = await page.content();
+
+  // Extract script tags
+  const scripts = await page.$$eval('script', scriptTags => 
+    scriptTags.map(script => {
+      return {
+        src: script.src || null,
+        content: script.src ? null : script.innerHTML
+      };
+    })
+  );
+
+  // Log the scripts
+  var myScripts = ""
+  scripts.forEach((script, id) => {
+    let curScript = ""
+    if (script.src) {
+      curScript = 'External script:', script.src , '\n';
+      writeToFile("External/"+id)
+    } else {
+      curScript = 'Inline script content:', script.content, '\n';
+      writeToFile("Inline/"+ id)
+    }
+    myScripts += curScript
+  });
+
+
+  // Write the HTML content to a file
+  fs.writeFile('body.html', html, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error dumping body:', err);
+    } else {
+      console.log('Successful write for html');
+    }
+  });
+
+// Write scripts content to a file
+fs.writeFile( "myScripts", myScripts, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error dumping body:', err);
+    } else {
+      console.log('Successful write for scripts');
+    }
+  });
+
+  
+ 
+  
 
   await browser.close();
 })();
